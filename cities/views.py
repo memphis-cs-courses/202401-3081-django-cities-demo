4ad995b24c320ce6a43c33ee11945b883f592f15
Katie Bridson <kbridson@memphis.edu>
2024-04-14 18:19:30 -0500

    Added mac postgresql setup and city index page

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    cities = ["Memphis", "Los Angeles", "Seattle", "Washington D.C.", "Orlando"]
+    context = {"cities": cities}
+    return render(request, "cities/home.html", context)
